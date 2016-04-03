3ca2a2c0a356cd042b41348cd8ee03b72869341d
Didier Baczynski <didier.baczynski@gmail.com>
2016-04-03 11:19:49 -0400

    Add numberSuite

---
+++ ListExo/src/main/java/org/bad/learningjava/NumberSuite.java
@@ -0,0 +1,16 @@
+package org.bad.learningjava;
+
+/**
+ * Created by didier_baczynski on 2016-04-03.
+ */
+public class NumberSuite {
+    public static void main(String[] args) {
+        int numberInSuite = 0;
+        int numberToAdd=1;
+        for (int i = 0; i <50 ; i++) {
+            numberInSuite=numberInSuite+numberToAdd;
+            numberToAdd=numberToAdd+2;
+            System.out.printf("Suite number[%d] is %d %n",i+1,numberInSuite);
+        }
+    }
+}
