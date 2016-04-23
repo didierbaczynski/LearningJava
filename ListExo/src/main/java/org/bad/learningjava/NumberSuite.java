06025e6ca012ebcc38fba0a9199d83002dd1b08b
Didier Baczynski <didier.baczynski@gmail.com>
2016-04-23 17:55:15 -0400

    Add numberSuite

--- ListExo/src/main/java/org/bad/learningjava/NumberSuite.java
+++ ListExo/src/main/java/org/bad/learningjava/NumberSuite.java
@@ -7,7 +7,7 @@ public class NumberSuite {
     public static void main(String[] args) {
         int numberInSuite = 0;
         int numberToAdd=1;
-        for (int i = 0; i <50 ; i++) {
+        for (int i = 0; i <100 ; i++) {
             numberInSuite=numberInSuite+numberToAdd;
             numberToAdd=numberToAdd+2;
             System.out.printf("Suite number[%d] is %d %n",i+1,numberInSuite);
